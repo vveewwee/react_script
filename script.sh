9e13744f7e58430ca7b9355bf97fa8fbb3f3ff77
vveewwee <vgeorgakakou@gmail.com>
2022-12-21 10:38:06 +0100

    added some color and opening file with vs code 2

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NORM='\033[0m'
+
 npx create-react-app $1
 npm add react-router-dom
 npm add styled-components
@@ -7,10 +11,14 @@ npm i --save @fortawesome/fontawesome-svg-core
 npm install --save @fortawesome/free-solid-svg-icons
 npm install --save @fortawesome/react-fontawesome
 npm install  --save-dev jsdoc
-npm install --save-dev redux
-npm install -save react-redux
-npm install --save-dev redux-devtools
+#npm install --save-dev redux
+#npm install -save react-redux
+#npm install --save-dev redux-devtools
 npm install axios
 #npm install --save-dev d3
 #npm install recharts
-echo $1 file is created
+
+
+echo  "${RED} $1 file is created ${NORMAL}"
+echo  "${GREEN} Now opening $1 on VS Code 2${NORMAL}"
+open -a Visual\ Studio\ Code\ 2 $1
